48e85aa58e2c7632cc835c13e455c597cc971070
Sandra Domagalska <sandradomagalska@wp.pl>
2018-03-10 15:50:55 +0100

    zmienne

---
+++ warsztaty/js/js/komentarze.js
@@ -0,0 +1,7 @@
+    //tak wygląda komentarz liniowy
+/*
+tutaj jest komentarz blokowy
+z kilku linii się składający
+
+// a tu zagnieżdżę sobie liniowy komentarz
+*/
---
+++ warsztaty/js/js/zmienne.js
@@ -0,0 +1,16 @@
+var imie;
+var nazwisko = "Nowacki";
+var wiek= 32;
+
+imie="Adam";
+
+
+/*
+console.log (imie);
+
+imie="Adam";
+console.log(imie);
+*/
+//łączenie zmiennych
+var laczenieTekstu="Moje imie to:"+imie+" a nazwisko to:" + nazwisko;
+console.log (laczenieTekstu);
