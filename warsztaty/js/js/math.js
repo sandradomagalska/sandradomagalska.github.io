e34099e1e9ef146b149de189cace0e808283d55a
Sandra Domagalska <sandradomagalska@wp.pl>
2018-03-11 14:01:39 +0100

    Math

---
+++ warsztaty/js/js/math.js
@@ -0,0 +1,9 @@
+// właściwości obiektu Math
+console.log(Math.E);
+console.log(Math.PI);
+console.log(Math.SQRT2);
+
+//metody obiektu Math
+console.log( Math.cos(30));
+console.log(Math.random());
+console.log(Math.pow(2,2));
