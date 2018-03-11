237c7705846590bc10a63fa6c57b553f30f4dd93
Sandra Domagalska <sandradomagalska@wp.pl>
2018-03-11 14:48:47 +0100

    operatory

---
+++ warsztaty/js/js/operatory.js
@@ -0,0 +1,70 @@
+//operatory arytmetyczne
+var liczba1 = 10;
+var liczba2 = 5;
+
+
+//dodawanie 
+var wynik = liczba1 + liczba2;
+
+//odejmowanie
+wynik = liczba1 - liczba2;
+
+// mnożenie
+wynik = liczba1*liczba2;
+
+//dzielenie
+wynik = liczba1/liczba2;
+
+//dzielenie modulo
+wynik = liczba1 % liczba2;
+
+//inkrementacja
+liczba2++;
+
+//dekrementacja
+liczba2--;
+
+//console.log (liczba2);
+
+
+
+//operatory przypisania
+
+var liczba3= 200;
+var liczba4= 100;
+var liczba5= 15;
+var liczba6= 40;
+var liczba7= 7;
+
+liczba3 += 200; //liczba3= liczba3+200;
+liczba4 -= 50; //liczba4=liczba4-50;
+liczba5 *= 2; //liczba5 = liczba5*2;
+liczba6 /= 2; //liczba6= liczba6/2
+liczba7 %= 2; //liczba7= liczba7%2;
+
+
+//console.log(liczba7);
+
+//operatory porowania
+
+var liczba8= "10";
+var liczba9= 10;
+
+if(liczba8===liczba9){
+    console.log("Warunek spełniony, czyli true");
+} else{
+    console.log("Warunek nie spełniony, czyli false");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
