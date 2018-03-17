e3d425d2dc7f869cbd5d5aaf387711b431079c61
Sandra Domagalska <sandradomagalska@wp.pl>
2018-03-17 17:00:13 +0100

    java script  zdarzenia

---
+++ warsztaty/DOM/js/zdarzenia.js
@@ -0,0 +1,64 @@
+//var paragraf = document.getElementById('par2');
+//
+////sposob 1
+//function obsluzZdarzenie(){
+//    console.log("click");
+//}
+//paragraf.onclick = obsluzZdarzenie;
+//
+//// sposob 2
+//paragraf.onclick = function(e) {
+//    console.log("Do obsługi zdarzenia przypisania funkcja anonimowa")
+//}
+
+////var counter =0;
+////paragraf.addEventListener("mouseenter", function(event){
+////    counter++;
+////    console.log(event.type);
+////    console.log("Zdarzenie wywołane "+ counter + " raz" ); 
+////});
+//
+//var title = document.getElementById("main-header");
+//title.addEventListener("mouseenter", function(){
+//    title.style.fontSize = "70px";
+//});
+//title.addEventListener("mouseleave", function(){
+//    title.style.fontSize ="20px";
+//})
+
+//function bodyColor(event) {
+//    console.log(event);
+//    document.body.style.backgroundColor = "red";
+//}
+//var title = document.getElementById("main-header");
+//title.addEventListener("click", bodyColor);
+//
+//
+////usuwanie obslugi zdarzen
+////title.removeEventListener("click", bodyColor);
+
+
+////blokowanie domyslngo działania elementów
+
+//document.querySelector('.link1').addEventListener('click', function(event){
+//    event.preventDefault();
+//console.log("Kliknąłem link klasy .link1")
+//});
+
+                              
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
