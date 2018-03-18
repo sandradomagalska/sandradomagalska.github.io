2952d6198bdb8bdf332f7184c5f6d976fdfafb9b
Sandra Domagalska <sandradomagalska@wp.pl>
2018-03-18 11:37:35 +0100

    jQuery1

---
+++ warsztaty/jQuery/js/main.js
@@ -0,0 +1,5 @@
+$(function(){
+    //$(".title").css({color :"red", backgroundColor: "yellow"});
+    // $("h2").css({color: "red", backgroundColor:"yellow"});
+    $("[class='title']").css ({color:"red", backgroundColor:"yellow"});
+});
