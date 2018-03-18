8a13c2396a1d21953ed050abf6ecebd2040baab8
Sandra Domagalska <sandradomagalska@wp.pl>
2018-03-18 12:02:50 +0100

    jQuery Selektory

--- warsztaty/jQuery/js/main.js
+++ warsztaty/jQuery/js/main.js
@@ -1,3 +1,6 @@
+//document.ready gwarantuje, że skrypty wywołują się w momencie gdy cały model DOM jest już załadowany
+//poniżej skrócony zapis document.ready
+
 $(function(){
     //$(".title").css({color :"red", backgroundColor: "yellow"});
     // $("h2").css({color: "red", backgroundColor:"yellow"});
